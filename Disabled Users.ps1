a02e36c0749299fc3194f0580ff532e179ed3533
Saggiehaim <saggiehaim@gmail.com>
2018-02-14 15:24:34 +0200

    Add files via upload

---
+++ Disabled Users.ps1
@@ -0,0 +1,27 @@
+##########################################################
+## This Script Written by Saggie Haim                   ##
+## The script is exporting all Disabled Users from the  ##
+## Active Directory, without the Service accounts       ##
+## Please dont make any changes if you don't know what  ##
+##                                                      ##
+##########################################################
+Import-Module ActiveDirectory
+
+$reportname = "DisabledUsers " + (Get-date -Format d-M-yy) + ".csv"
+$ReportLocation = "C:\Temp\" + $reportname
+$Report = @()
+
+$users = Get-ADUser -filter * -Properties EmployeeID,Department,Company,whenCreated | Where-object {$_.Enabled -eq $false}
+foreach ($user in $users) {
+    if ($user.EmployeeID -ne $null) {
+         $ReportLine = [PSCustomObject][Ordered]@{
+            'Full Name'           = $user.Name
+            'Employee ID'         = $user.EmployeeID
+            'Department'          = $user.Department
+            'Company'             = $user.Company
+            'When Created'        = $user.whenCreated
+        }
+    $Report += $ReportLine
+    }
+}
+$Report | export-csv -NoTypeInformation -Path $ReportLocation
