53a098a9c5ce0efafb4e790359c4b27b4b53f2f8
saggie haim <saggiehaim@gmail.com>
2019-01-05 03:53:33 +0200

    Rewrite Module

--- Unlinked GPOs/Get Unlinked GPO.ps1
+++ Unlinked GPOs/Get Unlinked GPO.ps1
@@ -1,55 +1,43 @@
 function get-GPOUnlinked () {
     [cmdletbinding()]
-        param (
-            [parameter(Position=0,
-            Mandatory=$false)]
-            [switch]$Delete = $false
-        )
-     $unlinkedGPO = @()
-     try {
+    param (
+        [parameter(Position = 0,
+            Mandatory = $false)]
+        [switch]$Delete = $false
+    )
+    $unlinkedGPO = @()
+    try {
         Write-Verbose -Message "Importing GroupPolicy module"
         Import-Module GroupPolicy -ErrorAction Stop
-     }
-     catch {
+    }
+    catch {
         Write-Error -Message "GroupPolicy Module not found. Make sure RSAT (Remote Server Admin Tools) is installed"
         exit
-     }
-     try {
+    }
+    try {
         Write-Verbose -Message "Importing GroupPolicy Policies"  
         $GPOs = Get-GPO -All  
-        Write-Verbose -Message "Found $GPOs.Count policies to check"
-     }
-     catch {
+        Write-Verbose -Message "Found '$($GPOs.Count)' policies to check"
+    }
+    catch {
         Write-Error -Message "Can't Load GPO's Please make sure you have connection to Domain Controllers"
         exit
-     }
-     ForEach($gpo  in $GPOs)
-     { 
-         Write-Verbose -Message "Checking $gpo.name link"
-         [xml]$GPOXMLReport = $gpo | Get-GPOReport -ReportType xml 
-         if ($GPOXMLReport.GPO.LinksTo -eq $null)
-         { 
-             $unlinkedGPO += $gpo 
-         }
-     }  
-     if ($Delete -eq $True)
-     {
-        foreach ($gpo in $unlinkedGPO)
-        {
-             $remove = $True
-             try {
-                 $gpo | Remove-GPO
-             }
-             catch {
-                 $remove = $false
-             }
-             if ($remove -eq $false)
-             {
-                 $Res += $gpo.name + " Remove failed"
-             }
-             Else {
-                 $Res += $gpo.name + " Remove Success"
-             }
-         }
-     }
-}
+    }
+    ForEach ($gpo  in $GPOs) { 
+        Write-Verbose -Message "Checking '$($gpo.DisplayName)' link"
+        [xml]$GPOXMLReport = $gpo | Get-GPOReport -ReportType xml
+        if ($null -eq $GPOXMLReport.GPO.LinksTo) { 
+            Write-Warning "'$($gpo.DisplayName)' is not linked" 
+        }
+        if ($null -eq $GPOXMLReport.gpo.User.ExtensionData -and $null -eq $GPOXMLReport.gpo.Computer.ExtensionData) {
+            Write-Warning "'$($gpo.DisplayName)' is empty"
+        }
+        If ($GPO.User.Enabled) {
+            $GPOPermissionForAuthUsers = Get-GPPermission -Guid $GPO.Id -All | select -ExpandProperty Trustee | ? {$_.Name -eq "Authenticated Users"}
+            $GPOPermissionForDomainComputers = Get-GPPermission -Guid $GPO.Id -All | select -ExpandProperty Trustee | ? {$_.Name -eq "Domain Computers"}
+            If (!$GPOPermissionForAuthUsers -and !$GPOPermissionForDomainComputers) {
+                Write-Warning "'$($GPo.DisplayName)' do not grant any permissions to the 'Authenticated Users' or 'Domain Computers' groups"
+            }
+        }
+    }
+}  
